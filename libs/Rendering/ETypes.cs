247be281585f333fe89bf59ba7bb9694c5cff868
FelixTeutsch <felix@teutsch.it>
2024-05-02 18:10:07 +0200

    Revert "Revert "Key added to list, sorry for the merge fuck up :(""
    This reverts commit 1bf99f68d54b8e996bb9967b217a0d599fe6da51.

--- libs/Rendering/ETypes.cs
+++ libs/Rendering/ETypes.cs
@@ -1,9 +1,10 @@
 public enum GameObjectType
 {
-  Player, // 0
-  Wall, // 1
-  Box, // 2
-  Floor, // 3
-  Goal, // 4
-  Obstacle // 5
+    Player, // 0
+    Wall, // 1
+    Box, // 2
+    Floor, // 3
+    Goal, // 4
+    Obstacle, // 5
+    Key // 6
 }
